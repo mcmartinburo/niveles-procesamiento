223f3d002a1257d4221a24f1f8647ce774bef0b5
MC Martín-Buro <42032448+mcmartinburo@users.noreply.github.com>
2026-01-20 14:08:50 +0100

    Update script.js
    add new items

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 const items = [
-    {id:1, categoria:"ESPECIAS", objetivo:"canela", condicion:"nrp"},
-    {id:2, categoria:"ANIMALES", objetivo:"caballo", condicion:"rp-"},
-    {id:3, categoria:"MÚSICA", objetivo:"guitarra", condicion:"nrp"},
-    {id:4, categoria:"PROFESIONES", objetivo:"bombero", condicion:"rp-"},
-    {id:5, categoria:"ESPECIAS", objetivo:"azafrán", condicion:"nrp"},
-    {id:6, categoria:"ANIMALES", objetivo:"tiburón", condicion:"rp-"},
-    {id:7, categoria:"INSTRUMENTOS", objetivo:"flauta", condicion:"nrp"},
-    {id:8, categoria:"ANIMALES", objetivo:"vaca", condicion:"rp-"},
-    {id:9, categoria:"ESPECIAS", objetivo:"perejil", condicion:"nrp"},
-    {id:10, categoria:"PROFESIONES", objetivo:"cocinero", condicion:"rp-"},
-    {id:11, categoria:"INSTRUMENTOS", objetivo:"tambor", condicion:"nrp"},
-    {id:12, categoria:"DEPORTE", objetivo:"ciclismo", condicion:"rp-"},
-    {id:13, categoria:"ESPECIAS", objetivo:"romero", condicion:"nrp"},
-    {id:14, categoria:"FRUTAS", objetivo:"banana", condicion:"rp-"},
-    {id:15, categoria:"INSTRUMENTOS", objetivo:"violín", condicion:"nrp"},
-    {id:16, categoria:"DEPORTE", objetivo:"atletismo", condicion:"rp-"},
-    {id:17, categoria:"ESPECIAS", objetivo:"tomillo", condicion:"nrp"},
-    {id:18, categoria:"FRUTAS", objetivo:"limón", condicion:"rp-"},
-    {id:19, categoria:"INSTRUMENTOS", objetivo:"saxofón", condicion:"nrp"},
-    {id:20, categoria:"PROFESIONES", objetivo:"fontanero", condicion:"rp-"},
-    {id:21, categoria:"ESPECIAS", objetivo:"orégano", condicion:"nrp"},
-    {id:22, categoria:"DEPORTE", objetivo:"patinaje", condicion:"rp-"},
-    {id:23, categoria:"INSTRUMENTOS", objetivo:"clarinete", condicion:"nrp"},
-    {id:24, categoria:"FRUTAS", objetivo:"melón", condicion:"rp-"},
-    {id:25, categoria:"DEPORTE", objetivo:"boxeo", condicion:"rp+"},
-    {id:26, categoria:"FRUTAS", objetivo:"naranja", condicion:"rp+"},
-    {id:27, categoria:"DEPORTE", objetivo:"tenis", condicion:"rp+"},
-    {id:28, categoria:"FRUTAS", objetivo:"fresa", condicion:"rp+"},
-    {id:29, categoria:"PROFESIONES", objetivo:"dentista", condicion:"rp+"},
-    {id:30, categoria:"DEPORTE", objetivo:"natación", condicion:"rp+"},
-    {id:31, categoria:"FRUTAS", objetivo:"piña", condicion:"rp+"},
-    {id:32, categoria:"ANIMALES", objetivo:"burro", condicion:"rp+"},
-    {id:33, categoria:"PROFESIONES", objetivo:"veterinario", condicion:"rp+"},
-    {id:34, categoria:"ANIMALES", objetivo:"paloma", condicion:"rp+"},
-    {id:35, categoria:"ANIMALES", objetivo:"oveja", condicion:"rp+"},
-    {id:36, categoria:"PROFESIONES", objetivo:"jardinero", condicion:"rp+"}
+    {id:1, objetivo:"regadera"},
+    {id:2, objetivo:"jirafa"},
+    {id:3, objetivo:"elefante"},
+    {id:4, objetivo:"saxofón"},
+    {id:5, objetivo:"cuchillo"},
+    {id:6, objetivo:"maleta"},
+    {id:7, objetivo:"zapatillas"},
+    {id:8, objetivo:"perro"},
+    {id:9, objetivo:"casa de madera"},
+    {id:10, objetivo:"camión"},
+    {id:11, objetivo:"flores amarillas"},
+    {id:12, objetivo:"estatua"},
+    {id:13, objetivo:"coche"},
+    {id:14, objetivo:"gato"},
+    {id:15, objetivo:"peluche"},
+    {id:16, objetivo:"cofre"},
+    {id:17, objetivo:"tenedor"},
+    {id:18, objetivo:"botella"},
+    {id:19, objetivo:"mariposa"},
+    {id:20, objetivo:"motorista"},
+    {id:21, objetivo:"cañón"},
+    {id:22, objetivo:"lazo"},  
 ];
 
 let chart = null;
@@ -47,7 +33,6 @@ function renderizarTablaItems() {
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${item.id}</td>
-            <td>${item.categoria}</td>
             <td>${item.objetivo}</td>
             <td><input type="number" min="0" max="1" id="resp-${index}" value="0"></td>
             <td><strong>${item.condicion.toUpperCase()}</strong></td>
